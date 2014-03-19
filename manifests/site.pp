0031f1469d35464bd4bd3bc264987c2a280d90cb
Luke Orland <orluke@gmail.com>
2014-03-18 23:24:28 -0400

    Add hipchat

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,6 +58,9 @@ node default {
   include hub
   include nginx
 
+  # my custom things
+  include hipchat
+
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
